96aaf37e07629e0e2606294d0210a3f50c93fbc2
Prashant <prashant.saini@myntra.com>
2019-09-23 19:17:42 +0530

    extend Scroll duration of ongoing animation.

---
+++ android/src/main/java/android/support/v4/view/MYNPagerAdapter.java
@@ -0,0 +1,11 @@
+package android.support.v4.view;
+
+import android.database.DataSetObserver;
+
+public abstract class MYNPagerAdapter extends PagerAdapter {
+
+    @Override
+    public void setViewPagerObserver(DataSetObserver observer) {
+        super.setViewPagerObserver(observer);
+    }
+}
